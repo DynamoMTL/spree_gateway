17bb1844a03150ad6451cc9963aa661f8d335a9a
Bryan Mahoney <bryan@goDynamo.com>
2014-09-11 18:30:07 -0400

    Removes skrill support from spree gateway

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 Spree::Core::Engine.add_routes do
   # Add your extension routes here
   resources :orders do
-    resource :checkout, :controller => 'checkout' do
-      member do
-        get :skrill_cancel
-        get :skrill_return
-      end
-    end
+    resource :checkout, :controller => 'checkout'
   end
-
-  post '/skrill' => 'skrill_status#update'
 end
